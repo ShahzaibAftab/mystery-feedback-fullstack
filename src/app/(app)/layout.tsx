0a1af9e25d34dea5a4eb4c9ee6ae09143017dcbf
ShahzaibAftab <shahhzaibaftab@gmail.com>
2024-05-18 13:55:01 +0500

    UI and Logics

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster"
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>
+          <Navbar />
           {children}
           <Toaster />
         </body>
